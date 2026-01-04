b8b0159629abc84431bad42d7e7f7f19167af906
Vivek <kandarivivek260@gmail.com>
2026-01-04 14:46:46 +0000

    init

---
+++ src/Raffle.s.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+ * @title Raffle
+ * @author Vivek
+ * @notice This contract is for creating a sample Raffle
+ */
+
+contract Raffle {
+
+}
