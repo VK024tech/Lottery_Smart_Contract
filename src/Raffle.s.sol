5eecb6bd3b907951f60614539f0ee92529ffb7d1
Vivek <kandarivivek260@gmail.com>
2026-01-10 12:21:06 +0000

    fund subscription

--- src/Raffle.s.sol
+++ src/Raffle.s.sol
@@ -168,11 +168,15 @@ contract Raffle is VRFConsumerBaseV2Plus {
 
     // Getter Functions
 
-    function entranceFee() external view returns (uint256) {
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 
     function getRaffleState() external view returns (RaffleState) {
         return s_raffleState;
+    }
+
+    function getPlayer(uint256 indexOfPlayer) external view returns(address) {
+        return s_players[indexOfPlayer];
     }
 }
